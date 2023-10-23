2c9da003c737a9737d5d48e59ae5a3bc7c060a61
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-10-23 19:47:19 +0400

    add Login and Register views

--- AviatoShop/AviatoShop/ViewModels/LoginVM.cs
+++ AviatoShop/AviatoShop/ViewModels/LoginVM.cs
@@ -1,6 +1,16 @@
-﻿namespace AviatoShop.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AviatoShop.ViewModels
 {
     public class LoginVM
     {
+        [Required(ErrorMessage = "Bu xana bosh ola bilmez!")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Bu xana bosh ola bilmez!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool IsRemember { get; set; }
+
     }
 }
