08830043a1cb8504447a010cd515b0a9fd9e20ed
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-10-23 20:23:39 +0400

    add Logout,CreateRoles and Register post method

--- AviatoShop/AviatoShop/Controllers/PagesController.cs
+++ AviatoShop/AviatoShop/Controllers/PagesController.cs
@@ -1,16 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AviatoShop.Helpers;
+using AviatoShop.Models;
+using AviatoShop.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AviatoShop.Controllers
 {
     public class PagesController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+
+        public PagesController(UserManager<AppUser> userManager,
+                               RoleManager<IdentityRole> roleManager,
+                               SignInManager<AppUser> signInManager) 
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
             return View();
         }
+
+        #region Register
         public IActionResult Register()
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            AppUser newUser = new()
+            {
+                Name = registerVM.Name,
+                Email = registerVM.Email,
+                Surname = registerVM.Surname,
+                UserName = registerVM.Username
+            };
+            IdentityResult identityResult = await _userManager.CreateAsync(newUser, registerVM.Password);
+            if (!identityResult.Succeeded)
+            {
+                foreach (IdentityError error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View();
+            }
+            await _userManager.AddToRoleAsync(newUser,Roles.Member.ToString());
+            await _signInManager.SignInAsync(newUser, registerVM.IsRemember);
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+
+        #region Logout
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        } 
+        #endregion
+
+        #region CreateRoles
+        //public async Task CreateRoles()
+        //{
+        //    if (!await _roleManager.RoleExistsAsync(Roles.Admin.ToString()))
+        //    {
+        //        await _roleManager.CreateAsync(new IdentityRole { Name = Roles.Admin.ToString() });
+        //    }
+        //    if (!await _roleManager.RoleExistsAsync(Roles.Member.ToString()))
+        //    {
+        //        await _roleManager.CreateAsync(new IdentityRole { Name = Roles.Member.ToString() });
+        //    }
+        //} 
+        #endregion
     }
 }
